86fdd5c32aa29b745264e597387a390833a9619b
josuess13 <josuess13@gmail.com>
2021-07-18 09:39:05 -0300

    Primeira alteração página dinâmica

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+include 'views/menu.html';
+include 'views/centro.html';
+include 'views/footer.html';
---
+++ pages/produtos.php
@@ -0,0 +1,11 @@
+<?php
+
+echo '<html> 
+<head>
+<link rel="stylesheet" href="../css/menu.css">
+<link rel="stylesheet" href="../css/footer.css">
+</head>
+</html>';
+include '../views/menu.html';
+include '../views/produtos.html';
+include '../views/footer.html';
