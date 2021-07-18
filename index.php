2d608e93597b3877b194db6040f3ac9a9a9383bb
josuess13 <josuess13@gmail.com>
2021-07-18 11:22:03 -0300

    Navegação entre páginas 2

--- index.php
+++ index.php
@@ -13,9 +13,20 @@ switch ($pagina) {
     case 'inicio':
         include 'views/centro.html';
         break;
-    
     case 'produtos':
         include 'views/produtos.html';
+        break;
+    case 'servicos':
+        include 'views/servicos.html';
+        break;
+    case 'vendas':
+        include 'views/vendas.html';
+        break;
+    case 'manutencao':
+        include 'views/manutencao.html';
+        break;
+    case 'clientes':
+        include 'views/clientes.html';
         break;
     default:
         include 'views/centro.html';
