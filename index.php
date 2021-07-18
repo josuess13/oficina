c8e01ea4496ac67824d53c149ebe6b48c92c50be
josuess13 <josuess13@gmail.com>
2021-07-18 11:18:09 -0300

    Navegar entre páginas 1

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
 
+$pagina = 'home';
+
+if (isset($_GET['i'])) {
+    $pagina = addslashes($_GET['i']);
+}
+
 include 'views/menu.html';
-include 'views/centro.html';
 include 'views/footer.html';
+
+switch ($pagina) {
+    case 'inicio':
+        include 'views/centro.html';
+        break;
+    
+    case 'produtos':
+        include 'views/produtos.html';
+        break;
+    default:
+        include 'views/centro.html';
+        break;
+}
