d566cc36262faee02b6397d5601d9b781c293a44
Your Name <josuess13@gmail.com>
2021-08-05 20:07:18 -0300

    voltando php para html, alteração tela produtos

--- index.php
+++ index.php
@@ -6,29 +6,29 @@ if (isset($_GET['i'])) {
     $pagina = addslashes($_GET['i']);
 }
 
-include 'views/menu.php';
-include 'views/footer.php';
+include 'views/menu.html';
+include 'views/footer.html';
 
 switch ($pagina) {
     case 'inicio':
-        include 'views/centro.php';
+        include 'views/centro.html';
         break;
     case 'produtos':
-        include 'views/produtos.php';
+        include 'views/produtos.html';
         break;
     case 'servicos':
-        include 'views/servicos.php';
+        include 'views/servicos.html';
         break;
     case 'vendas':
-        include 'views/vendas.php';
+        include 'views/vendas.html';
         break;
     case 'manutencao':
-        include 'views/manutencao.php';
+        include 'views/manutencao.html';
         break;
     case 'clientes':
-        include 'views/clientes.php';
+        include 'views/clientes.html';
         break;
     default:
-        include 'views/centro.php';
+        include 'views/centro.html';
         break;
 }
